64275b7ae8db1692a9ea274340bd04826d4e6580
sriram9531 <sammymommy009@gmail.com>
2024-04-30 22:00:39 +0530

    Balaji completed

--- src/test/java/org/test/Test1.java
+++ src/test/java/org/test/Test1.java
@@ -5,7 +5,13 @@ import org.testng.annotations.Test;
 public class Test1 {
 
 	@Test
-	private void test() {
-		
+	private void test1() {
+		System.out.println("Day1");
 	}
+	
+	@Test
+	private void test2() {
+		System.out.println("Day2");
+	}
+	
 }
