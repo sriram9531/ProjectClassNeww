d7fd0d32d176edc7df0560b10924daa7b5a6c765
sriram9531 <sammymommy009@gmail.com>
2024-05-06 10:40:07 +0530

    sriram completed

--- src/test/java/org/test/Test1.java
+++ src/test/java/org/test/Test1.java
@@ -4,6 +4,9 @@ import org.testng.annotations.Test;
 
 public class Test1 {
 
+	public static void main(String[] args) {
+		System.out.println("HI");
+	}
 	@Test
 	private void test1() {
 		System.out.println("Day1");
@@ -12,6 +15,11 @@ public class Test1 {
 	@Test
 	private void test2() {
 		System.out.println("Day2");
+	}
+	
+	@Test
+	private void test3() {
+		System.out.println("Day3");
 	}
 	
 }
---
+++ src/test/java/org/test/Test2.java
@@ -0,0 +1,12 @@
+package org.test;
+
+import org.testng.annotations.Test;
+
+public class Test2 {
+	
+	@Test
+	private void hi11() {
+		System.out.println("Hii");
+	}
+
+}
